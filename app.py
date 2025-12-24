7b9e7762c31f91a7c6f60a6ed3b7356737921d28
Mahmoud46 <mahmoudzakaria443@gmail.com>
2025-12-24 19:22:10 +0200

    Update

--- app.py
+++ app.py
@@ -1,13 +1,11 @@
-from flask import Flask, render_template
+from flask import Flask
 from controllers.request_controller import request_bp
+from controllers.home_controller import home_bp
 
 app = Flask(__name__)
 
 
-@app.route('/')
-def main():
-    return render_template('index.html')
-
+app.register_blueprint(home_bp)
 app.register_blueprint(request_bp)
 
 if (__name__ == '__main__'):
